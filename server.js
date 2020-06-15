7b95007f277380aac0d0bccf2807a9627fc54922
davidbacter01 <davidbacter01@gmail.com>
2020-06-15 05:37:54 +0300

    project done

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-// Setup empty JS object to act as endpoint for all routes
 const projectData = {};
-
-// Require Express to run server and routes
-const express = require('express');
-
-// Start up an instance of app
+projectData.data = [];
+const express = require("express");
+//start an instance of the app
 const app = express();
+//setting the port
+const port = 3000;
+/* Dependencies */
+const bodyParser = require("body-parser");
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
-// Initialize the main project folder
-app.use(express.static('website'));
 
+/* Initializing the main project folder */
+app.use(express.static("website"));
+//app.get("/", (req, res) => res.send("Hello World!"));
 
-// Setup Server
-const port = 3000;
-const server = app.listen(port, () => console.log(`server.js is listening at http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
+
+app.get("/", (req, res) => res.send("index.html"));
+app.get("/all", (req, res) => res.send(projectData));
+app.post("/data", (req, res) => {
+  projectData.data.push(req.body);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,74 @@
-/* Global Variables */
+const url = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey = ",us&appid=b6141bf22492492309146aeb7cbae5fb";
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+//setting the route to post data to the server
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Body data type must match "Content-Type" header
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+//setting the route to get data from api
+const getWeather = async (url, zip, apiKey) => {
+  zip = document.getElementById("zip").value;
+  let apiUrl = url + zip + apiKey;
+  const res = await fetch(apiUrl);
+  try {
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+};
+
+//setting the ui updating prommise
+const updateUI = async () => {
+  const request = await fetch("/all");
+  try {
+    const allData = await request.json();
+    const lastEntry = allData.data[allData.data.length - 1];
+    document.getElementById("date").innerHTML = `Date: ${lastEntry.date}`;
+    document.getElementById(
+      "temp"
+    ).innerHTML = `Temperature: ${lastEntry.temperature}`;
+    document.getElementById(
+      "content"
+    ).innerHTML = `What you felt at the time: ${lastEntry.userResponse}`;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+//adding the event listener  to get data from api and chain promisses
+document.getElementById("generate").addEventListener("click", processData);
+
+function processData() {
+  getWeather(url, zip, apiKey)
+    .then((data) => {
+      const newData = {};
+      newData.temperature = data.main.temp;
+      newData.date = getDateAndTime();
+      newData.userResponse = document.getElementById("feelings").value;
+      postData("/data", newData);
+    })
+    .then(updateUI);
+}
+
+function getDateAndTime() {
+  return new Date().toLocaleString();
+}
